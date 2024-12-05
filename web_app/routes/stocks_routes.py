65e9dddd1ba5d0b847813b350f4d292fb9473f82
anwinn03 <atn24@georgetown.edu>
2024-12-04 23:49:14 -0500

    Updates

--- web_app/routes/stocks_routes.py
+++ web_app/routes/stocks_routes.py
@@ -7,13 +7,12 @@ from app.stocks import fetch_stocks_csv, format_usd
 
 stocks_routes = Blueprint("stocks_routes", __name__)
 
-
 @stocks_routes.route("/stocks/form")
 def stocks_form():
     print("STOCKS FORM...")
     return render_template("stocks_form.html")
 
-
+#@stocks_routes.route("/stocks/dashboard", methods=["POST"])
 @stocks_routes.route("/stocks/dashboard", methods=["GET", "POST"])
 def stocks_dashboard():
     print("STOCKS DASHBOARD...")
@@ -21,21 +20,23 @@ def stocks_dashboard():
     if request.method == "POST":
         # for data sent via POST request, form inputs are in request.form:
         request_data = dict(request.form)
+        print("FORM DATA:", request_data)
     else:
         # for data sent via GET request, url params are in request.args
-        request_data = dict(request.args)
+        request_data = dict(request.args) # /stocks/dashboard?symbol=GOOGL
+        print("URL PARAMS:", request_data)
 
-    print("REQUEST DATA:", request_data)
-
-    symbol = request_data.get("symbol") or "NFLX" # get specified symbol or use default
+    # grabbing some data from the dictionary sent by the form (or url params)
+    #risk_level = request_data.get("risk_level")
+    symbol = request_data.get("symbol") or "NFLX"
 
     try:
         df = fetch_stocks_csv(symbol=symbol)
         latest_close_usd = format_usd(df.iloc[0]["adjusted_close"])
         latest_date = df.iloc[0]["timestamp"]
-        data = df.to_dict("records")
+        data = df.to_dict("records") # list of dictionaries!!
 
-        #flash("Fetched Real-time Market Data!", "success")
+        flash("Fetched Real-time Market Data!", "success")
         return render_template("stocks_dashboard.html",
             symbol=symbol,
             latest_close_usd=latest_close_usd,
@@ -47,7 +48,6 @@ def stocks_dashboard():
 
         flash("Market Data Error. Please check your symbol and try again!", "danger")
         return redirect("/stocks/form")
-
 
 #
 # API ROUTES
@@ -64,8 +64,8 @@ def stocks_api():
 
     try:
         df = fetch_stocks_csv(symbol=symbol)
-        data = df.to_dict("records") # convert dataframe to list of dict
-        return {"symbol": symbol, "data": data}
+        data = df.to_dict("records")
+        return {"symbol": symbol, "data": data }
     except Exception as err:
         print('OOPS', err)
         return {"message":"Market Data Error. Please try again."}, 404
